a504b460ca31608d343b4b9da4f23ddf63536461
Rafael Ferreira Machado <166616125+Rafael-Ferr-Dev@users.noreply.github.com>
2024-04-26 16:48:45 -0300

    v2

---
+++ js/index.js
@@ -0,0 +1,20 @@
+export function handleNavScroll() {
+    const nav = document.getElementById('nav');
+    const button_nav = document.getElementById('button_nav');
+    const scrollPosition = window.scrollY;
+    const topPosition = nav.getBoundingClientRect().top;
+
+    if (topPosition <= 0 && scrollPosition > 0) {
+        nav.classList.add('nav-fixed');
+        nav.classList.remove('nav-top');
+        button_nav.classList.add('button_nav_fixed');
+        button_nav.classList.remove('button_nav');
+    } else {
+        nav.classList.remove('nav-fixed');
+        nav.classList.add('nav-top');
+        button_nav.classList.add('button_nav');
+        button_nav.classList.remove('button_nav_fixed');
+    }
+}
+
+window.addEventListener('scroll', handleNavScroll);
