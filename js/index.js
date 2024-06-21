40f6d1264a0c5bf75a41986650c4c018f178e0e1
Rafael Ferreira Machado <166616125+Rafael-Ferr-Dev@users.noreply.github.com>
2024-06-20 21:15:40 -0300

    v40

--- js/index.js
+++ js/index.js
@@ -14,18 +14,18 @@ function handleNavScroll() {
         if( window.matchMedia('(max-width: 768px)').matches){
             ocupation.style.height = "32vh"
         }else{
-            ocupation.style.height = "45vh"
+            ocupation.style.height = "264px"
         }
     } else {
         nav.classList.remove('nav-fixed');
         nav.classList.add('nav-top');
         button_nav.classList.add('button_nav');
         button_nav.classList.remove('button_nav_fixed');
-        ul_nav.style.position = 'absolute';
+        ul_nav.style.position = 'relative';
         if( window.matchMedia('(max-width: 768px)').matches){
              ocupation.style.height = "27vh"
         }else{
-            ocupation.style.height = "45vh"
+            ocupation.style.height = "264px"
         }
     }
 }
